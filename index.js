86c6b79720fd3dc56613949c4ffd8943e2da0afb
Guilherme <guilherme.silva@mpt.mp.br>
2023-02-06 15:49:12 -0300

    2022-02-06 15:49

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require("express")
-const fs = require("fs")
-const path = require("path")
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
-const app = express()
-const secretKey = "secret-key"
+const app = express();
+const secretKey = "secret-key";
 
 app.get("/pdf/:filename", (req, res) => {
-    const { filename } = req.params
-    const filePath = path.join(__dirname, "files", filename)
-    console.log(req.params)
-    if (req.query.key !== secretKey) {
-        return res.status(401).send("Unauthorized")
-    }
+  const { filename } = req.params;
+  const filePath = path.join(__dirname, "files", filename);
+  console.log(filePath);
+  if (req.query.key !== secretKey) {
+    return res.status(401).send("Unauthorized");
+  }
 
-    if (!fs.existsSync(filePath)) {
-        return res.status(404).send("File not found")
-    }
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).send("File not found");
+  }
 
-    res.contentType("application/pdf")
-    res.sendFile(filePath)
-})
+  res.contentType("application/pdf");
+  res.sendFile(filePath);
+});
 
-app.listen(3000, () => console.log("Listening on port 3000"))
+app.listen(3000, () => console.log("Listening on port 3000"));
